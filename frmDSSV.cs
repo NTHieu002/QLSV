aba64cc7e11262d92249786fb3573e20832ab0b7
NTHieu002 <nguyentrunghieu002@gmail.com>
2022-05-25 07:54:52 +0700

    first commit

---
+++ frmDSSV.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLSV_3layers
+{
+    public partial class frmDSSV : Form
+    {
+        public frmDSSV()
+        {
+            InitializeComponent();
+        }
+
+        private string tukhoa="";
+        private void frmDSSV_Load(object sender, EventArgs e)
+        {
+            LoadDSSV();//gọi tới hàm load sinh viên khi form đc load
+        }
+
+        private void LoadDSSV()
+        {
+            //load toàn bộ danh sách sinh viên khi form được load
+
+            //khai báo list customparameter --> xem part 3 để hiểu rõ hơn
+            List<CustomParameter> lstPara = new List<CustomParameter>();
+            lstPara.Add(new CustomParameter()
+            {
+                key = "@tukhoa",
+                value = tukhoa
+            });
+            dgvSinhVien.DataSource = new Database().SelectData("SelectAllSinhVien",lstPara);
+
+            //đặt tên cột
+            dgvSinhVien.Columns["masinhvien"].HeaderText = "Mã SV";
+            dgvSinhVien.Columns["hoten"].HeaderText = "Họ tên";
+            dgvSinhVien.Columns["nsinh"].HeaderText = "Ngày sinh";
+            dgvSinhVien.Columns["gt"].HeaderText = "Giới tính";
+            dgvSinhVien.Columns["quequan"].HeaderText = "Quê quán";
+            dgvSinhVien.Columns["diachi"].HeaderText = "Địa chỉ";
+            dgvSinhVien.Columns["email"].HeaderText = "Email";
+            dgvSinhVien.Columns["dienthoai"].HeaderText = "Điện thoại";
+        }
+
+        private void dgvSinhVien_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            if(e.RowIndex>=0)
+            {
+                var msv = dgvSinhVien.Rows[e.RowIndex].Cells["masinhvien"].Value.ToString();
+                //cần truyền mã sinh viên này vào form sinh viên
+                new frmSinhVien(msv).ShowDialog();
+
+                LoadDSSV();
+            }
+        }
+
+        private void btnThemmoi_Click(object sender, EventArgs e)
+        {
+            new frmSinhVien(null).ShowDialog();
+            LoadDSSV();
+        }
+
+        private void btnTimkiem_Click(object sender, EventArgs e)
+        {
+            tukhoa = txtTukhoa.Text;
+            LoadDSSV();
+        }
+    }
+}
